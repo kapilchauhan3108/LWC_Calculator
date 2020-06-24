8609e2d2a29ebe9b4f94e4f4c3136872ee41273d
Kapil Singh. Chauhan <kapil.chauhan@AD.SAASFOCUS.COM>
2020-06-24 10:54:19 +0530

    final

--- force-app/main/default/lwc/sampleLwc/sampleLwc.js
+++ force-app/main/default/lwc/sampleLwc/sampleLwc.js
@@ -1,3 +1,24 @@
-import { LightningElement } from 'lwc';
+import { LightningElement ,track } from 'lwc';
 
-export default class SampleLwc extends LightningElement {}
+export default class SampleLwc extends LightningElement {
+
+@track   dynamicGreeting = "World";
+
+@track displayDiv = false ;
+
+changeDynamicGreeting (event){
+
+    console.log('event.target  : ' , event.target);
+
+this.dynamicGreeting = event.target.value;
+}
+
+
+checkboxChange(event){
+
+    this.displayDiv =  event.target.checked ;
+}
+
+@track cityList = ['Jaipur' , 'Delhi'];
+
+}
